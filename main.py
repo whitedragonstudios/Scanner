747d696efb6b71b971c372a50792b098c4c4cd78
whitedragonstudios <rowens.wds@gmail.com>
2025-10-15 19:57:58 -0400

    added config file for easy settings changes adjusted html, css and server to use config file function for loading config file in server.py needs to be moved to main.py later made css tweaks

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from datetime import datetime as dt
+import json
 
+
+# Load a config file which allows users to easily change settings
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 # Event handler for opening Database
 
@@ -9,6 +14,7 @@ from datetime import datetime as dt
 
 # Update flask server with person output
 
+# Send clock in time to storage DB 
 
 if __name__ == "__main__":
     pass
--- server.py
+++ server.py
@@ -1,23 +1,44 @@
 from flask import Flask, render_template, request
 from datetime import datetime as dt
+import json
+import os
+
+
+# Load a config file which allows users to easily change settings NOTE this may be changed to postgre DB in the future. 
+def load_config():
+    config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.json')
+    try:
+        with open(config_path,"r") as config_file:
+            config = json.load(config_file)
+            print("Config file loaded.")
+    except FileNotFoundError:
+        config = {"placeholder_key": "placeholder_value"}n
+        print("Config file not found. Using default settings.") 
+    return config
+
 
 # Intialize flask server
 app = Flask(__name__)
 
+config = load_config()
 # Set default and index route
 @app.route ('/')
-@app.route('/index')
+#@app.route('/index')
 # Render updates from person to webpage
 def update_webpage():
     # Get text from input box on webpage
     scanned_id = request.args.get('idnumber')
+    if scanned_id == None:
+        scanned_id = "No ID"
     # Pass idnumber to person object. Person object returns name, group, time, and image (if availible)
     return render_template("index.html", 
-                           name="Bob",
-                           group="First shift", 
+                           idnumber=scanned_id,
+                           name="Marcus Aurelius",
+                           group="Emperor of Rome", 
                            date=dt.now().strftime("%m-%d-%y"),
                            time=dt.now().strftime("%H:%M"),
-                           picture = "default.jpg"
+                           picture = "default.jpg",
+                           cf = config
                            )
 
 if __name__ == "__main__":
