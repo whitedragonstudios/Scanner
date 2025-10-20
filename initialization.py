f66b166d2603030cc9b1a2ac22045b62f94fd736
whitedragonstudios <rowens.wds@gmail.com>
2025-10-20 19:16:18 -0400

    Started settings menu framework
    made settings object to store config values as well as update them in the database
    created rough menu.htlm page for future settings menu
    modified initialization.py to include settings object
    made flowchart of modules and their interactions
    updated sprintLog.txt (major designs changes for next sprint)

--- initialization.py
+++ initialization.py
@@ -10,25 +10,71 @@ import pandas as pd
 postgressql_host = "localhost"
 postgressql_employee_db = "people"
 postgressql_timesheet_db = "timesheet"
+postgressql_role_db= "role"
+postgressql_config = "config"
 postgressql_user = "marcus"
 postgressql_password = "stoic"
 
-
 # Load a config file which allows users to easily change settings NOTE this may be changed to postgre DB in the future. 
-def load_config():
-    config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.json')
-    try:
-        with open(config_path,"r") as config_file:
-            config = json.load(config_file)
-            print("Config file loaded.")
-    except FileNotFoundError:
-        config = {"placeholder_key": "placeholder_value"}
-        print("Config file not found. Using default settings.") 
-    return config
+#def load_config():
+#    config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.json')
+#    try:
+#        with open(config_path,"r") as config_file:
+#            config = json.load(config_file)
+#            print("Config file loaded.")
+#    except FileNotFoundError:
+#        config = {"placeholder_key": "placeholder_value"}
+#        print("Config file not found. Using default settings.") 
+#    return config
 
-config = load_config()
+# config = load_config()
 
-paths = [config.get("CSV_path"), config.get("XLSX_path"), config.get("JSON_path")]
+
+class Setting:
+    def __init__(self):
+        data = self.request_config()
+        self.config_status = data["config_status"]
+        self.config_date = data["config_date"]
+        self.CSV_path = data["CSV_path"]
+        self.XLSX_path = data["XLSX_path"]
+        self.JSON_path = data["JSON_path"]
+        self.webpage_title = data["webpage_title"]
+        self.company = data["company"]
+        self.mainbackground_color = data["main_background_color"]
+        self. main_text_color = data["main_text_color"]
+        self.button_color = data["button_color"]
+        self.button_text_color = data["button_text_color"]
+        self.button_border_color = data["button_border_color"]
+        self.button_border_hover_color = data["button_border_hover_color"]
+        self.button_border_color = data["sidebar_color"]
+        self.sidebar_text_color = data["sidebar_text_color"]
+    def connect_db(self):
+        connect = psycopg2.connect(
+            host = postgressql_host,
+            dbname = postgressql_config,
+            user = postgressql_user,
+            password = postgressql_password
+            )
+        return connect
+    def request_config(self):
+        db=self.connect_db()
+        with db.cursor() as cursor:
+            cursor.execute("SELECT * FROM config_table;")
+            config_data = cursor.fetchall()
+            config = {row[0]: row[1] for row in config_data}
+        db.close()
+        return config
+    def update_config(self, key, value):   
+        db = self.connect_db
+        with db.cursor() as cursor:
+            cursor.execute("UPDATE config_table SET value = %s WHERE key = %s;", (value, key))
+            db.commit()
+        db.close()
+
+        
+setting = Setting()
+
+paths = setting.CSV_path, setting.XLSX_path, setting.JSON_path
 print(paths)
 for item in paths:
     if item is not None:
---
+++ settings.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from datetime import datetime as dt
+
+from initialization import Setting
+
+setting = Setting()
+# Intialize flask server
+app = Flask(__name__)
+
+# Set menu route
+@app.route ('/menu')
+
+# Render updates from setting to webpage
+def update_webpage():
+    # Get text from input box on webpage
+    new_company_name = request.args.get('new_companyname')
+
+    return render_template("menu.html", 
+                           company_name=setting.company,
+                           cf = setting
+                           )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2000)
+    #serve(app, host="0.0.0.0", port=2000))
