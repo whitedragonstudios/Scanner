991edf2d3063d653d437f20cd0f3289989ca29e6
whitedragonstudios <rowens.wds@gmail.com>
2025-10-21 22:33:16 -0400

    Added comments and testing tools

--- createDB.sql
+++ createDB.sql
@@ -1,14 +1,14 @@
+---This SQL script creates the necessary database and tables for the Scanner application.
+-- It sets up the config_database, people_database, and timesheet_database tables,
+-- and inserts initial configuration data into the config_database table.
 
-
-
-GRANT ALL PRIVILEGES ON DATABASE scanner TO marcus;
-
-
+-- Create config_database table to hold configuration settings
 CREATE TABLE config_database (
     key VARCHAR(50) PRIMARY KEY,
     value VARCHAR(128)
 );
-GRANT ALL PRIVILEGES ON TABLE config_database TO marcus;
+--- Insert default configuration data into config_database 
+--- If option 3 was chosen from psql_setup.sh this data will NOT be overwritten.
 INSERT INTO config_database (key, value) VALUES
     ('config_status', 'False'),
     ('config_date', '2025-01-01'),
@@ -26,6 +26,8 @@ INSERT INTO config_database (key, value) VALUES
     ('sidebar_color','#222222'),
     ('sidebar_text_color','#f5f5f5');
 
+--- Create people_database table to hold employee information 
+--- Data can be inserted via the web interface run by setttings.py
 CREATE TABLE people_database (
     id SERIAL PRIMARY KEY, 
     employee_id INTEGER UNIQUE,
@@ -38,31 +40,47 @@ CREATE TABLE people_database (
     position VARCHAR(50),
     department VARCHAR(50)
     );
-GRANT ALL PRIVILEGES ON TABLE people_database TO marcus;
 
+--- Create timesheet_database table to hold clock-in and clock-out records.
+--- Reports will be generated from this data. 
 CREATE TABLE timesheet_database (
     id SERIAL PRIMARY KEY,
-    employee_id INTEGER NOT NULL REFERENCES people_database(employee_id) ON DELETE CASCADE,
-    clock_in TIMESTAMPTZ,
-    clock_out TIMESTAMPTZ,
-    work_date DATE
+    employee_id INTEGER NOT NULL REFERENCES people_database(employee_id) ON DELETE CASCADE, -- Foreign key reference to people_database query can only be made using employee_id
+    clock_in TIMESTAMPTZ DEFAULT NOW(),
+    clock_out TIMESTAMPTZ DEFAULT NOW(),
+    work_date DATE DEFAULT CURRENT_DATE
     );
+
+-- marcus will be the main user for the scanner database tables are made with defult postgres user but we need to give marcus access to them.
+GRANT ALL PRIVILEGES ON TABLE config_database TO marcus;
+GRANT ALL PRIVILEGES ON TABLE people_database TO marcus;
 GRANT ALL PRIVILEGES ON TABLE timesheet_database TO marcus;
 
+--- Update config status to True and set config date to current date 
+--- main.py uses these two values to determine if initial setup has been completed.
+--- These values can be reset in the setttings.py web interface re-enabling launch of initial setup. 
+UPDATE config_database SET value = 'True' WHERE key = 'config_status';
+UPDATE config_database SET value = CURRENT_DATE WHERE key = 'config_date';
+
+--- Feedback for user to verify tables were created successfully
 \dt
 
+--- Below this line are options to verify database functionality. They will be left in for advanced users. 
+
+-- Uncomment next line to verify config data insertion
+-- SELECT * FROM config_database LIMIT (SELECT COUNT(*) FROM config_database)
+
 -- Uncomment next three line to insert test data into people_database
---INSERT INTO people_database (employee_id, first_name, last_name, email, phone, pic_path, employee_role, position, department) VALUES
-    --(2001, 'Jojo', 'Bob', 'jbob@scanner.com', '100-555-1234', '/images/2001.jpg', 'Testsubject', 'CTO', 'Thing-a-ma-jig Development'),
+-- INSERT INTO people_database (employee_id, first_name, last_name, email, phone, pic_path, employee_role, position, department) VALUES
+    --(2001, 'Han', 'Solo', 'hsolo@scanner.com', '100-555-1976', '/images/2001.jpg', 'Scoundrel', 'Pilot', 'Only in it for the money'),
     --(2002, 'Luke', 'Skywalker', 'lskywalker@scanner.com', '100-555-1978', '/images/2002.jpg', 'Jedi Master', 'Like his father', 'Peace and Justice');
 
 -- Uncomment next three line to insert test data into timesheet_database
---INSERT INTO timesheet_database (employee_id, clock_in, clock_out, work_date) VALUES
+-- INSERT INTO timesheet_database (employee_id, clock_in, clock_out, work_date) VALUES
     --(2001, '2025-10-01 08:00:00+00', '2025-10-01 16:00:00+00', '2025-10-01'),
     --(2002, '2025-10-01 08:00:00+00', '2025-10-01 16:00:00+00', '2025-10-01');
 
--- Uncomment next three lines to verify data insertion
-SELECT * FROM config_database;
-SELECT * FROM people_database;
-SELECT * FROM timesheet_database;
-
+-- Uncomment next three lines to verify data schemas
+-- \d+ config_database; SELECT * FROM config_database;
+-- \d+ people_database; SELECT * FROM people_database;
+-- \d+ timesheet_database; SELECT * FROM timesheet_database;
--- psql_setup.sh
+++ psql_setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# change these variables as needed to create a custom user and database
+# change these variables as needed to create a custom user, port, and database names advanced users only
 user="marcus"
 port=5000
 dbname="scanner"
 
-
+# Prompt user to create a password for the PostgreSQL user
+# In current itteration password is reset each time script is run. For security this will need to be changed in future versions.
 echo "Create password for your PostgreSQL databases"
 while true; do
-    read -sp "Enter password: " password #stoic
+    read -sp "Enter password: " password #stoic is the deault password for the development environment
     echo
     read -sp "Confirm password: " password2
     echo
@@ -21,6 +22,7 @@ while true; do
     fi
 done
 
+# Prompt user to install PostgreSQL if not already installed. 
 choice=""
 while true; do
     read -sp "Install PostgreSQL? (y/n): " choice
@@ -38,10 +40,12 @@ while true; do
     fi
 done
 
+# Start PostgreSQL service if not already running
 sudo service postgresql start
 echo "PostgreSQL service started."
 
-
+# Create PostgreSQL user and database if they do not already exist
+# Here the passwrod is reset. Higher security implementations will be added in future versions.
 sudo -u postgres psql -p "$port" -d postgres -c "
 DO \$\$
 BEGIN
@@ -56,6 +60,8 @@ END
 \$\$;
 "
 
+
+# Create database if it does not already exist
 echo "'$dbname' exists..."
 sudo -u postgres psql -p "$port" -d postgres -c "
 DO \$\$
@@ -66,16 +72,20 @@ BEGIN
 END
 \$\$;
 "
-
+# Grant all privileges on database to user. After setup user marcus will be used to access the database.
 sudo -u postgres psql -p "$port" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${dbname} TO ${user};"
 
+# Prompt user to choose how to handle existing tables. If there is a catastrophic error users may want to restore only config settings or keep all existing data.
+# Or if data for employees completely changes a clean re-install may be desired. 
+# Finally option 3 is the default to keep all existing data. If the tables do not exist they will be created fresh in createDB.sql
 echo "Choose which tables you want to reset"
-echo "  1) CLEAN re-install - DROP all tables (WARNING: will delete all data stored in people & timesheets)"
-echo "  2) RESET configuration settings only (set to defaults)"
+echo "  1) CLEAN re-install - Delete all tables (WARNING: will delete all data stored in people & timesheets)"
+echo "  2) RESET configuration settings only (people and timesheet data will be kept)"
 echo "  3) KEEP all tables (choose this to keep all existing data)"
 read -p "Enter 1, 2 or 3: " choice
 choice=${choice:-3}
 
+# Conditional logic to drop tables based on user choice
 case "$choice" in
     1)
         echo "Performing CLEAN re-install"
@@ -90,16 +100,19 @@ case "$choice" in
             DROP TABLE IF EXISTS config_database CASCADE;"
         ;;
     3)
-        echo "Keeping existing tables (no drops)."
+        echo "Keeping existing tables."
         ;;
     *)
         echo "Invalid choice - keeping existing tables."
         ;;
 esac
 
-
+# Run createDB.sql to complete the table setup. 
 echo "Creating tables as needed..."
 sudo -u postgres psql -p "$port" -d "$dbname" -f createDB.sql
+# Table creation is seperated in this module so that shell scripts for other platfroms can be created.
 
 
+# After completing createDB.sql this script finishes
 echo "Setup complete."
+echo "Your PostGreSQL datebase: $dbname is configured and ready to be used by: $user on port: $port"
