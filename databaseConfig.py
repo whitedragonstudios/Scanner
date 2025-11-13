a81c8386b102c8f9de4d60e4e83931d176bed586
whitedragonstudios <rowens.wds@gmail.com>
2025-11-12 23:50:08 -0500

    Added admin credentials to databaseConfig

--- databaseConfig.py
+++ databaseConfig.py
@@ -1,9 +1,14 @@
-"""Default setitng for the Postgre Database"""
-
-
+#Default setitng for the Postgre Database
 def databaseSettings():
     return {"user" :"marcus",
         "password" : "stoic",
         "db_name" : "scanner",
+        "port" : 5000,
+        "host" : "localhost"}
+
+def databseAdmin(database = "postgres"):
+    return {"user" :"postgres",
+        "password" : "stoic",
+        "db_name" : database or "postgres",
         "port" : 5000,
         "host" : "localhost"}
