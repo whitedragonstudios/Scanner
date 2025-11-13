b794223417a556d5ed2e044358301234a2fc3702
whitedragonstudios <rowens.wds@gmail.com>
2025-11-12 23:45:48 -0500

    updated comments on classweather

--- classweather.py
+++ classweather.py
@@ -11,17 +11,22 @@ db_name = db["db_name"]
 port = db["port"]
 host = db["host"]
 
+# weather report uses an api to send weather to the flask interface
 class weather_report():
     def __init__(self, city_name, weather_key):
+        # city name and weather key come from config database.
         self.city_name = city_name
         self.weather_key = weather_key
+        # API needs gps coordinates so city has to be passed to get_gps
         gps = self.get_gps(self.city_name)
         self.longitude = gps[0]
         self.latitude = gps[1]
         self.city = gps[2]
         self.state = gps[3]
         self.country = gps[4]
+        # Once gps is retrieved a second call to get_weather is performed
         data = self.get_weather()
+        # response is formatted for direct output.
         self.description = data['weather'][0]['description'].title()
         self.icon = data['weather'][0]['icon'] + ".png"
         self.feel = int((data['main']['feels_like']) * 1.8 - 459.67)
@@ -32,40 +37,49 @@ class weather_report():
         self.clouds = data['clouds']['all']
         dir = self.wind_direction(data['wind']['deg'])
         self.wind = f"{dir} {int(data['wind']['speed'])}mp/h "
+        # config database is updated with changes to match new city
         self.update_config()
 
 
+    # Get gps passes city to return long and lat
     def get_gps(self,city):
-        try: 
+        try:
+            # requests api response
             GPS_response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={self.weather_key}").json()
             #print(GPS_response)
             if not GPS_response:
                 print(f"WARNING: No results found for city '{city}'. Using default (New York City).")
+                # Returns usable list with default data sets
                 return [-74.0060152, 40.7127281, "New York", "NY", "US"]
-
+            # Extract list and dictionary to get usable data.
             country = GPS_response[0].get('country', 'US')
             state = GPS_response[0].get('state', '')
             name = GPS_response[0].get('name', city)
             lon = GPS_response[0].get('lon', -74.0060152)
             lat = GPS_response[0].get('lat', 40.7127281)
+            # return a list
             return [lon, lat, name, state, country]
         
         except requests.exceptions.RequestException as e:
             print("ERROR: weather.get_gps >>> api request >>>", e)
-            GPS_response = [-74.0060152, 40.7127281, "New York", "NY", "US"]
-            return GPS_response
+            # Returns usable list with default data sets
+            return [-74.0060152, 40.7127281, "New York", "NY", "US"]
     
 
+    #Get weather makes second call to the api for detailed weather 
     def get_weather(self):
+        # api call uses gps coordinates from get gps
         try: 
             WEATHER_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&appid={self.weather_key}").json()
         except requests.exceptions.RequestException as e:
             print("ERROR: weather.get_weather >>> api request >>>", e)
-            WEATHER_response = "Error Weather Data"
+            WEATHER_response = "Error Weather Data" # should add default data response
         #print(WEATHER_response)
+        # Returns full dict
         return WEATHER_response
     
 
+    # update config sends gps city and country data to the config database
     def update_config(self):
         conn = Handler(user, password, db_name, port, host)
         conn.update_config('city', self.city)
@@ -73,7 +87,7 @@ class weather_report():
         conn.update_config('lat', self.latitude)
         conn.update_config('country', self.country)
     
-
+    # Weather direction translates degrees returned from api to cardinal directions.
     def wind_direction(self, wind_dir):
         try:
             wind_float = float(wind_dir)
@@ -86,7 +100,8 @@ class weather_report():
             elif wind_float in range(293, 336): cardinal = 'NW'
             else: cardinal = 'N' # 0-22 and 337-360
         except: 
-            cardinal = ""
+            # returns a blank string
+            cardinal = " "
             print("ERROR: (wind_direction) assigning >>> wind_dir")
         # print("CF --- wind_direction RUN")
         return cardinal
